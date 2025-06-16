980040c7f3a44bfac0320c321873f28bd825b250
MarcosFelype <mfs22@aluno.ifal.edu.br>
2025-06-16 11:56:52 -0300

    /fix: Alterando nomenclatura das estruturas do projeto para o português.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-import { ManagerModule } from './manager/manager.module';
-import { EmployeeModule } from './employee/employee.module';
-import { TaskEmployeePerspectiveModule } from './taskEmployeePerspective/task.employee.perspective.module';
-import { TaskManagerPerspectiveModule } from './taskManagerPerspective/task.manager.perspective.module';
-import { TaskModule } from './task/task.module';
-import { CoinsModule } from './coins/coins.module';
+import { UsuarioModule } from './usuario/usuario.module';
+import { TarefaVisaoGestorModule } from './tarefaVisaoGestor/tarefa.visao.gestor.module';
+import { TarefaVisaoColaboradorModule } from './tarefaVisaoColaborador/tarefa.visao.colaborador.module';
+import { GestorModule } from './gestor/gestor.module';
+import { ColaboradorModule } from './colaborador/colaborador.module';
+import { TarefaModule } from './tarefa/tarefa.module';
+import { MoedasModule } from './moedas/moedas.module';
 
 
 @Module({
-  imports: [UserModule, ManagerModule, EmployeeModule, TaskEmployeePerspectiveModule, TaskManagerPerspectiveModule, TaskModule, ManagerModule, EmployeeModule, CoinsModule],
+  imports: [UsuarioModule, GestorModule, ColaboradorModule, TarefaVisaoColaboradorModule, TarefaVisaoGestorModule, TarefaModule, GestorModule, MoedasModule],
   controllers: [],
   providers: [],
 })
--- src/usuario/usuario.controller.ts
+++ src/usuario/usuario.controller.ts
@@ -1,6 +1,6 @@
 import { Controller } from "@nestjs/common";
 
 @Controller()
-export class UserController{
+export class UsuarioController{
 
 }

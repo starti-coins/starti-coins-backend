bb78f2871f3b0739ec1ac756f1b0ad9aa07cd810
Marcos Felype Silva <103426684+MarcosFelype@users.noreply.github.com>
2025-06-17 11:00:40 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
-import { TarefaVisaoGestorModule } from './tarefaVisaoGestor/tarefa.visao.gestor.module';
-import { TarefaVisaoColaboradorModule } from './tarefaVisaoColaborador/tarefa.visao.colaborador.module';
-import { GestorModule } from './gestor/gestor.module';
-import { ColaboradorModule } from './colaborador/colaborador.module';
-import { TarefaModule } from './tarefa/tarefa.module';
-import { MoedasModule } from './moedas/moedas.module';
-
 
 @Module({
-  imports: [UsuarioModule, GestorModule, ColaboradorModule, TarefaVisaoColaboradorModule, TarefaVisaoGestorModule, TarefaModule, GestorModule, MoedasModule],
+  imports: [UsuarioModule],
   controllers: [],
   providers: [],
 })

cbf42d5f8874b418de4e4aa2e6753b6b286a367e
MarcosFelype <mfs22@aluno.ifal.edu.br>
2025-06-25 11:56:19 -0300

    Incicio configuracao redis

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
+  const redis = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
+    transport: Transport.REDIS,
+    options: {
+      host: 'localhost',
+      port: 6379,
+    },
+  });
+
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
+
+
--- src/usuario/usuario.service.ts
+++ src/usuario/usuario.service.ts
@@ -1,6 +1,55 @@
-import { Injectable } from "@nestjs/common";
+import { MailerService } from "@nestjs-modules/mailer/dist";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import * as bcrypt from 'bcrypt'
+import { randomBytes } from 'crypto';
 
 @Injectable()
 export class UsuarioService {
+    
+    constructor(private readonly mailer: MailerService,
+        private readonly prisma: PrismaService
+    ) {}
 
+    //obs: alterar depois conforme o diagrama de classes
+    //atualmente utilizando conforme configuração do curso da HCode
+    async enviarEmailRedefinicaoSenha(email: string){
+        const user = await this.prisma.user.findFirst({ //preciso do banco de dados de user
+            where: {
+                email
+            }
+        });
+
+        if (!user){
+            throw new UnauthorizedException("E-mail incorreto. ");
+        }
+
+        //ciar assinatura do token (via redis)
+        const token = randomBytes(8).toString('hex'); //gera uma string com 16 caracteres
+
+        await this.mailer.sendMail({
+            subject: 'Recuperação de senha',
+            to: 'mfs53@aluno.ifal.edu.br',
+            html: `<p> Olá. Troque sua senha a partir deste token: ${token} </p>`
+        })
+    }
+
+    //adicionado o parâmetro {id} em relação ao diagrama de classes. Modificar depois
+    async redefinirSenha(id: number, token: string){
+
+
+        const salto = await bcrypt.genSalt();
+        const senhaHash = await bcrypt.hash(this.prisma.user.findFirst(id).senha, salto )
+
+        const usuario = await this.prisma.update({
+            where: {
+                id,
+            },
+            data: {
+                senhaHash
+            }
+        });
+
+        return this.criarToken(usuario) //token de acesso à conta;
+    }
 }

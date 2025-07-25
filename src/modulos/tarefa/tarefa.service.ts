5430fbd86e1422dcbd46dd33f0e996fb3cc47b75
MarcosFelype <mfs22@aluno.ifal.edu.br>
2025-07-25 14:05:29 -0300

    /fix: Alterando o shema prisma de tarefas

--- src/modulos/tarefa/tarefa.service.ts
+++ src/modulos/tarefa/tarefa.service.ts
@@ -12,14 +12,29 @@ export class TarefaService {
         try{
             const tarefa = await this.prisma.tarefas.create({
             data: {
-                //faltando data limite e projetos
-                ...taskData
-            }
-            })
+                titulo: taskData.titulo,
+                descricao: taskData.descricao,
+                id_colaborador: taskData.id_colaborador,
+                data_limite: taskData.data_limite,
+                data_atribuicao: taskData.dataAtribuicao,
+                quantidade_horas: taskData.quantidadeHoras,
+                quantidade_moedas: taskData.quantidadeMoedas
+
+            },
+        });
+            const tarefa_gestor_colaborador = await this.prisma.tarefa_gestor_colaborador.create({
+                data: {
+                    id_tarefa: tarefa.id_tarefa,
+                    id_gestor: 0,
+                    id_colaborador: tarefa.id_colaborador,
+                }
+            });
             return tarefa;
         }
         catch (error){
             throw new BadRequestException(`Erro ao criar tarefa: ${error.message}`);
         }
     }
+
+    
 }

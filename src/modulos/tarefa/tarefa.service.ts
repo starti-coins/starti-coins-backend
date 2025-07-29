ff9b9bcf95c6119dcb201c3e3b72f1e993535a1c
MarcosFelype <mfs22@aluno.ifal.edu.br>
2025-07-29 12:36:18 -0300

    /fix: Alterando o endpoint de criação de tarefa

--- src/modulos/tarefa/tarefa.service.ts
+++ src/modulos/tarefa/tarefa.service.ts
@@ -1,6 +1,7 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { CreateTaskDto } from "./dto/criar-tarefa.dto";
 import { PrismaService } from "src/prisma/prisma.service";
+import { gestores, tarefas } from "@prisma/client";
 
 @Injectable()
 export class TarefaService {
@@ -10,14 +11,28 @@ export class TarefaService {
         const {...taskData} = createTaskDto;
 
         try{
+            const colaborador = await this.prisma.colaboradores.findUnique({
+                where: {
+                    id_colaborador: taskData.id_colaborador
+                }
+            });
+
+            if (!colaborador) {
+                throw new NotFoundException(`Colaborador com ID ${taskData.id_colaborador} não encontrado.`);
+            }
+
+            const quantidadeHorasCalculada = await this.calcularQuantidadeHoras(taskData.dataAtribuicao, taskData.data_limite) * 2;
+            console.log('Quantidade de horas calculada: ', quantidadeHorasCalculada);
+
             const tarefa = await this.prisma.tarefas.create({
             data: {
                 titulo: taskData.titulo,
                 descricao: taskData.descricao,
-                data_limite: taskData.data_limite,
+                id_colaborador: taskData.id_colaborador,
                 data_atribuicao: taskData.dataAtribuicao,
-                quantidade_horas: taskData.quantidadeHoras,
-                quantidade_moedas: taskData.quantidadeMoedas
+                data_limite: taskData.data_limite,
+                quantidade_horas: quantidadeHorasCalculada,
+                quantidade_moedas: quantidadeHorasCalculada * 10
             },
         });
             return tarefa;
@@ -25,6 +40,26 @@ export class TarefaService {
         catch (error){
             throw new BadRequestException(`Erro ao criar tarefa: ${error.message}`);
         }
+    }
+
+    async inserirTarefaGestorColaborador(tarefa: tarefas, techLead: gestores){
+        if (tarefa.id_colaborador != null && techLead.id_gestor != null) {
+            const tarefaGestorColaborador = await this.prisma.tarefa_gestor_colaborador.create({
+            data: {
+                id_colaborador: tarefa.id_colaborador,
+                id_gestor: techLead.id_gestor,
+                id_tarefa: tarefa.id_tarefa,
+            }
+        });
+        }
+    }
+
+    private async calcularQuantidadeHoras(dataAtribuicao: Date, data_limite: Date) {
+        const umDiaEmMs = 24 * 60 * 60 * 1000;
+        const dataLimiteTime = new Date(data_limite).getTime();
+        const dataAtribuicaoTime = new Date(dataAtribuicao).getTime();
+        const diferencaDatasEmMs = Math.abs(dataLimiteTime - dataAtribuicaoTime);
+        return Math.floor(diferencaDatasEmMs / umDiaEmMs);
     }
 
     
